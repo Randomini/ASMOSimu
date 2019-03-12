6ebe826b040e909734bf270e07402326a5bf9fb1
Randomini <luca.kabbe@googlemail.com>
2019-03-12 23:16:16 +0100

    logik für Situationen hinterlegt

--- ASMOSimu/Dialog.xaml.cs
+++ ASMOSimu/Dialog.xaml.cs
@@ -14,6 +14,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.IO;
+using System.Speech.Synthesis;
 
 
 
@@ -24,7 +25,10 @@ namespace ASMOSimu
     /// </summary>
     public partial class Dialog : Page
     {
-       
+        public Random random = new Random();
+        public static Situation currentsit = new Situation();
+        public static Output currentout = new Output();
+        SpeechSynthesizer speechSynthesizer = new SpeechSynthesizer();
         public Dialog()
         {
             InitializeComponent();
@@ -33,8 +37,9 @@ namespace ASMOSimu
 
         private void Btn_StartSimu_Click(object sender, RoutedEventArgs e)
         {
-            LstVw_Situation.Items.Refresh();
-
+            currentout = currentsit.lst_outputs.ElementAt(random.Next(0, currentsit.lst_outputs.Count));
+            TxtBl_Output.Text = currentout.OutputText;
+            speechSynthesizer.SpeakAsync(currentout.OutputText);
         }
 
         private void Page_Loaded(object sender, RoutedEventArgs e)
@@ -43,6 +48,32 @@ namespace ASMOSimu
             LstVw_Situation.ItemsSource = CreateSituation.lst_situ;
             LstVw_Situation.Items.Refresh();
 
+        }
+
+        private void LstVw_Situation_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            currentsit = (Situation)LstVw_Situation.SelectedItem;
+        }
+
+        private void Btn_InputSent_Click(object sender, RoutedEventArgs e)
+        {
+            foreach(Input x in currentout.lst_inputs)
+            {
+                if (TxtBx_Input.Text.Contains(x.InputText))
+                {
+                    foreach(Situation y in CreateSituation.lst_situ)
+                    {
+                        if (y.Sit_Name.Equals(x.Next_Situation))
+                        {
+                            LstVw_Situation.SelectedItem = y;
+                            currentsit = y;
+                            currentout = currentsit.lst_outputs.ElementAt(random.Next(0, currentsit.lst_outputs.Count));
+                            TxtBl_Output.Text = currentout.OutputText;
+                            speechSynthesizer.SpeakAsync(currentout.OutputText);
+                        }
+                    }
+                }
+            }
         }
     }
 }
